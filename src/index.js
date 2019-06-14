a160e6bd8cbf4e3ff836381586da4658dbc2f12a
omrgrcn <omergercin@gmail.com>
2019-06-15 00:22:41 +0300

    creation of directory structure

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, combineReducers } from 'redux';
 
-const userReducer = (state='', action) => state;
-const productReducer = (state=[], action) => state;
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import userReducer from './reducers/userReducer';
+import productReducer from './reducers/productReducer';
 
 const rootReducer = combineReducers({
     products: productReducer,
@@ -39,7 +41,7 @@ const action = {
 
 store.dispatch(action);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
