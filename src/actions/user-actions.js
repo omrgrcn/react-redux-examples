b83b1bccb6d1c6208aae3715c2cf30dca8cef6c7
omrgrcn <omergercin@gmail.com>
2019-06-15 20:04:22 +0300

    async/await api call

--- src/actions/user-actions.js
+++ src/actions/user-actions.js
@@ -13,10 +13,16 @@ export function updateUser(newUser) {
 }
 
 export function getUsers() {
-    return dispatch => {
-        axios.get('https://jsonplaceholder.typicode.com/users/5')
+    return async dispatch => {
+        try {
+            const res = await axios.get('https://jsonplaceholder.typicode.com/users/5');
+            dispatch(updateUser(res.data.name));
+        } catch {
+            dispatch(showError());
+        }
+        /* axios.get('https://jsonplaceholder.typicode.com/users/5')
             .then(response => dispatch(updateUser(response.data.name)))
-            .catch(error => dispatch(showError()))
+            .catch(error => dispatch(showError())) */
     }
 }
 
